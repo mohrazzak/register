4c18ca4095d8b5433595bf536600c6b541dcf9b3
Mohammad Abdalrazzak <xxiimonsteriixx20@gmail.com>
2024-12-23 10:47:34 +0300

    almost

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,11 @@
 	let { children } = $props();
 </script>
 
+<enhanced:img
+	src={bg}
+	alt="Hero background"
+	class="fixed left-0 top-0 -z-10 h-full w-full object-cover blur-sm brightness-[.3]"
+/>
 <!-- Main Content -->
 <Header />
 <main>
