2b354a4fe927b687ee7b37d3230d3aa592769617
Mohammad Abdalrazzak <xxiimonsteriixx20@gmail.com>
2024-12-23 11:13:16 +0300

    a

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,16 @@ const config = {
 		alias: {
 			'@/*': './path/to/lib/*'
 		},
-		adapter: adapter()
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
 	}
 };
 
